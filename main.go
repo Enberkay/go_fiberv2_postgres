805b0f71fc2b0ad407827ac05825505f9beae3a1
apiwat <apiwat.ka65@snru.ac.th>
2025-04-22 12:49:34 +0700

    add createProduct func

--- main.go
+++ main.go
@@ -16,17 +16,27 @@ const (
 	password     = "mypassword"
 )
 
+var db *sql.DB
+
+type Product struct {
+	ID    int
+	Name  string
+	Price int
+}
+
 func main() {
 	// Connection string
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, username, password, databaseName)
 
-	db, err := sql.Open("postgres", psqlInfo)
+	sdb, err := sql.Open("postgres", psqlInfo)
 
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	db = sdb
 
 	err = db.Ping()
 
@@ -35,6 +45,23 @@ func main() {
 	}
 
 	//Connection Database Successful.
-	print("Connection Database Successful.")
+	fmt.Println("Connection Database Successful.")
 
+	err = createProduct(&Product{Name: "Go product 2", Price: 444})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Create Successful !")
+}
+
+func createProduct(product *Product) error {
+
+	_, err := db.Exec(
+		"INSERT INTO public.products(name, price)VALUES ($1, $2);",
+		product.Name,
+		product.Price,
+	)
+
+	return err
 }
