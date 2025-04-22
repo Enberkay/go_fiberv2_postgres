fd0d356ab290ae682886ed53dab763279cc9ee44
apiwat <apiwat.ka65@snru.ac.th>
2025-04-22 14:21:50 +0700

    getProductHandler with fiberv2

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
 
 	_ "github.com/lib/pq"
 )
@@ -19,9 +22,9 @@ const (
 var db *sql.DB
 
 type Product struct {
-	ID    int
-	Name  string
-	Price int
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Price int    `json:"price"`
 }
 
 func main() {
@@ -46,113 +49,24 @@ func main() {
 		log.Fatal(err)
 	}
 
-	//Connection Database Successful.
-	fmt.Println("Connection Database Successful.")
+	app := fiber.New()
 
-	// err = createProduct(&Product{Name: "Go product 2", Price: 444})
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println("Create Successful !")
+	app.Get("/product/:id", getProductHandler)
 
-	// product, err := getProduct(2)
-	// fmt.Println("Get Successful !", product)
+	app.Listen(":8080")
 
-	products, err := getProducts()
+}
+
+func getProductHandler(c *fiber.Ctx) error {
+	productId, err := strconv.Atoi(c.Params("id"))
 	if err != nil {
-		log.Fatal(err)
+		return c.SendStatus(fiber.StatusBadRequest)
 	}
-	fmt.Println(products)
-
-	// Product, err := updateProduct(4, &Product{Name: "Clerk", Price: 555})
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println("Update product Successful !", Product)
-
-	// err = deleteProduct(5)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println("Delete product Successful !")
-}
-
-func createProduct(product *Product) error {
-
-	_, err := db.Exec(
-		"INSERT INTO public.products(name, price)VALUES ($1, $2);",
-		product.Name,
-		product.Price,
-	)
-
-	return err
-}
-
-func getProduct(id int) (Product, error) {
-	var p Product
-	row := db.QueryRow(
-		"SELECT id,name,price FROM products WHERE id=$1;",
-		id,
-	)
-
-	err := row.Scan(&p.ID, &p.Name, &p.Price)
+	product, err := getProduct(productId)
 
 	if err != nil {
-		return Product{}, err
+		return c.SendStatus(fiber.StatusBadRequest)
 	}
 
-	return p, nil
-}
-
-func getProducts() ([]Product, error) {
-	rows, err := db.Query(
-		"SELECT id,name,price FROM products;",
-	)
-
-	if err != nil {
-		return nil, err
-	}
-
-	var products []Product
-
-	for rows.Next() {
-		var p Product
-		err := rows.Scan(&p.ID, &p.Name, &p.Price)
-		if err != nil {
-			return nil, err
-		}
-		products = append(products, p)
-	}
-
-	if err = rows.Err(); err != nil {
-		return nil, err
-	}
-
-	return products, nil
-}
-
-func updateProduct(id int, product *Product) (Product, error) {
-	var p Product
-	row := db.QueryRow(
-		"UPDATE public.products SET name=$1, price=$2 WHERE id=$3 RETURNING id, name, price;",
-		product.Name,
-		product.Price,
-		id,
-	)
-
-	err := row.Scan(&p.ID, &p.Name, &p.Price)
-
-	if err != nil {
-		return Product{}, err
-	}
-
-	return p, err
-}
-
-func deleteProduct(id int) error {
-	_, err := db.Exec(
-		"DELETE FROM public.products WHERE id=$1;",
-		id,
-	)
-	return err
+	return c.JSON(product)
 }
